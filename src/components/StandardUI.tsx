7a7c15f55cd9d6f737df1efbdb13b14d3fcc85a4
Rukaiya <rukaiyah678@gmail.com>
2025-12-22 15:02:06 +0530

    changes

--- src/components/StandardUI.tsx
+++ src/components/StandardUI.tsx
@@ -248,8 +248,8 @@ export function StandardUI({
 
   //
   //https://hostingate-client.vercel.app/sign-in https://app.hostingate.com/dashboard/profile
-  const API_BASE_URL = "https://app.hostingate.com/api/clientCustomerChatBox";
-  //const API_BASE_URL = "https://app.hertzora.ai/api/clientCustomerChatBox";
+  // const API_BASE_URL = "https://app.hostingate.com/api/clientCustomerChatBox";
+  const API_BASE_URL = "https://app.hertzora.ai/api/clientCustomerChatBox";
 
   // const API_BASE_URL = "http://localhost:3000/api/clientCustomerChatBox";
   useEffect(() => {
