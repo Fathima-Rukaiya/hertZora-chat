93436f2519f5f65e5e9a08fdebb4f9fec4f3d9be
Rukaiya <rukaiyah678@gmail.com>
2026-01-02 10:45:14 +0530

    changes

--- src/components/chatUI.tsx
+++ src/components/chatUI.tsx
@@ -234,7 +234,7 @@ const darkBorderColor = botColors ? darkenColor(botColors[2], 20) : "#50484cff";
                     ) : (
                         <Bot strokeWidth={1.75} size={22} />
                     )}
-                    <span className="font-semibold text-sm">Ask {botName}!
+                    <span className="font-semibold text-sm">{botName}!
                         
                     </span>
                 </button>
